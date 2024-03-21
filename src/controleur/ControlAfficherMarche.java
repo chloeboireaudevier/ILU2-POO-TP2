92217f2d1a9f1ef751fbd27d5e5865a1fef5e6c6
chloeboireaudevier <cbd65@hotmail.fr>
2024-03-21 14:55:46 +0100

    TP2-7 terminé

--- src/controleur/ControlAfficherMarche.java
+++ src/controleur/ControlAfficherMarche.java
@@ -9,5 +9,8 @@ public class ControlAfficherMarche {
 		this.village = village;
 	}
 
-	//TODO a completer
+	public String[] donnerEtatMarche() {
+		String[] infosMarche = this.village.donnerEtatMarche();
+		return infosMarche;
+	}
 }
